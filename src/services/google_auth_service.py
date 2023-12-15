992846a19a3add33ea0b4c2a7a54845756407cff
Evgeni Petrov <evgeni.petrov@gmail.com>
2023-12-15 20:58:41 +0200

    Added automatic login if secrets file is not found

--- src/services/google_auth_service.py
+++ src/services/google_auth_service.py
@@ -1,12 +1,11 @@
 import logging
 import os
 import pickle
+import webbrowser
 
 from django.conf import settings
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
-
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +17,7 @@ class GoogleAuthService:
 
     # Constants
     CLIENT_SECRET_FILE = os.path.join(settings.BASE_DIR, "..", "secrets", "client_secret.json")
+    GOOGLE_DEV_CONSOLE_URL = "https://console.developers.google.com/"
 
     SCOPES = [
         "https://www.googleapis.com/auth/webmasters.readonly",
@@ -33,10 +33,14 @@ class GoogleAuthService:
             if creds and creds.expired and creds.refresh_token:
                 logger.info("Refreshing Google auth credentials")
                 creds.refresh(Request())
-            else:
+            elif os.path.exists(self.CLIENT_SECRET_FILE):
                 logger.info("Getting new Google auth credentials")
                 flow = InstalledAppFlow.from_client_secrets_file(self.CLIENT_SECRET_FILE, self.SCOPES)
                 creds = flow.run_local_server(port=0)
+            else:
+                logger.error("client_secret.json not found. Please login to Google Dev Console and download it.")
+                webbrowser.open(self.GOOGLE_DEV_CONSOLE_URL)
+                return None
 
             self.__save_credentials(creds, self.token_file_path)
             logger.info("Google auth credentials saved")
