d87611700bbfc9868280b4e2872d4520aaa390bc
Evgeni Petrov <evgeni.petrov@gmail.com>
2023-12-17 21:47:35 +0200

    Cleanup

--- src/workflows/process_project_data_workflow.py
+++ src/workflows/process_project_data_workflow.py
@@ -12,39 +12,6 @@ class ProcessProjectDataWorkflow:
 
         self.url_data_processor = UrlDataProcessor(project)
 
-        # googleanalytics4
-        self.googleanalytics4_last_14m_data = None
-        self.googleanalytics4_last_1m_data = None
-        self.googleanalytics4_last_1m_previous_year_data = None
-        self.googleanalytics4_previous_1m_data = None
-        # googlesearchconsole
-        self.googlesearchconsole_date_page_query_last_16m_data = None
-        self.googlesearchconsole_page_last_16m_data = None
-        self.googlesearchconsole_page_last_1m_data = None
-        self.googlesearchconsole_page_last_1m_previous_year_data = None
-        self.googlesearchconsole_page_previous_1m_data = None
-        self.googlesearchconsole_page_query_last_16m_data = None
-        self.googlesearchconsole_page_query_last_1m_data = None
-        self.googlesearchconsole_page_query_last_1m_previous_year_data = None
-        self.googlesearchconsole_page_query_previous_1m_data = None
-        self.googlesearchconsole_query_last_16m_data = None
-        self.googlesearchconsole_query_last_1m_data = None
-        self.googlesearchconsole_query_last_1m_previous_year_data = None
-        self.googlesearchconsole_query_previous_15m_data = None
-        self.googlesearchconsole_query_previous_1m_data = None
-        # screamingfrog
-        self.screamingfrog_list_crawl_data = None
-        self.screamingfrog_sitemap_crawl_data = None
-        self.screamingfrog_spider_crawl_data = None
-        # semrush
-        self.semrush_analytics_backlinks_rootdomain_data = None
-        self.semrush_analytics_organic_competitors_data = None
-        self.semrush_analytics_organic_pages_data = None
-        self.semrush_analytics_organic_positions_rootdomain_data = None
-        # sitebulb
-        self.sitebulb_list_crawl_url_internal_data = None
-        self.sitebulb_spider_crawl_url_internal_data = None
-
     def run(self):
         """
         Main method to run the workflow.
