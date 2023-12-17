849702a60a96161e39676e7f2bd7b0007c90af25
Evgeni Petrov <evgeni.petrov@gmail.com>
2023-12-17 12:12:11 +0200

    Changed GSC timeout/retry routine to exponential

--- src/services/google_search_console_service.py
+++ src/services/google_search_console_service.py
@@ -1,17 +1,19 @@
 import datetime
+import logging
+
 import pandas as pd
 import requests.exceptions
 from googleapiclient.discovery import build
-from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
-
 from services.google_auth_service import GoogleAuthService
-
-import logging
+from tenacity import retry, retry_if_exception_type, stop_after_attempt
+from tenacity import wait_exponential
 
 logger = logging.getLogger(__name__)
 
+
 class GSCFetchError(Exception):
     pass
+
 
 class GoogleSearchConsoleService:
     ROW_LIMIT = 25000
@@ -25,7 +27,15 @@ class GoogleSearchConsoleService:
             raise GSCFetchError("Authentication failed") from e
 
     @staticmethod
-    def _create_request(start_date, end_date, dimensions, start_row, dimension_filter_groups=None, aggregation_type=None, data_type=None):
+    def _create_request(
+        start_date,
+        end_date,
+        dimensions,
+        start_row,
+        dimension_filter_groups=None,
+        aggregation_type=None,
+        data_type=None,
+    ):
         request = {
             "startDate": start_date,
             "endDate": end_date,
@@ -49,10 +59,18 @@ class GoogleSearchConsoleService:
         creds = auth_service.authenticate()
         return build("webmasters", "v3", credentials=creds)
 
-    @retry(stop=stop_after_attempt(10), wait=wait_fixed(5), retry=retry_if_exception_type(requests.exceptions.ReadTimeout))
+    @retry(
+        stop=stop_after_attempt(10),
+        wait=wait_exponential(multiplier=1, max=60),
+        retry=retry_if_exception_type(requests.exceptions.ReadTimeout),
+    )
     def execute_request(self, site_url, request_body) -> list:
         try:
-            response = self.service.searchanalytics().query(siteUrl=site_url, body=request_body).execute()
+            response = (
+                self.service.searchanalytics()
+                .query(siteUrl=site_url, body=request_body)
+                .execute()
+            )
             logger.info("Search Analytics query executed successfully.")
             return response.get("rows", [])
         except Exception as e:
@@ -64,15 +82,19 @@ class GoogleSearchConsoleService:
         flat_entry = {}
         for dim, value in zip(dimensions, entry["keys"]):
             flat_entry[dim] = value
-        flat_entry.update({
-            "clicks": entry["clicks"],
-            "impressions": entry["impressions"],
-            "ctr": round(entry["ctr"], 2),
-            "position": round(entry["position"], 1),
-        })
+        flat_entry.update(
+            {
+                "clicks": entry["clicks"],
+                "impressions": entry["impressions"],
+                "ctr": round(entry["ctr"], 2),
+                "position": round(entry["position"], 1),
+            }
+        )
         return flat_entry
 
-    def fetch_data(self, site_url, start_date: datetime.date, end_date: datetime.date, dimensions) -> pd.DataFrame:
+    def fetch_data(
+        self, site_url, start_date: datetime.date, end_date: datetime.date, dimensions
+    ) -> pd.DataFrame:
         all_data = []
         start_row = 0
 
@@ -84,7 +106,9 @@ class GoogleSearchConsoleService:
                 start_row,
             )
             try:
-                logger.info(f"Fetching GSC data from {start_date} to {end_date} for {site_url}, dimensions {dimensions}, start row {start_row}")
+                logger.info(
+                    f"Fetching GSC data from {start_date} to {end_date} for {site_url}, dimensions {dimensions}, start row {start_row}"
+                )
                 data = self.execute_request(site_url, request_body)
             except GSCFetchError as e:
                 logger.error(str(e))
@@ -103,9 +127,11 @@ class GoogleSearchConsoleService:
             start_row += len(data)
 
         logger.info(f"Fetched {len(all_data)} rows of GSC data")
-        return pd.DataFrame(all_data).astype({
-            "clicks": "int",
-            "impressions": "int",
-            "ctr": "float64",
-            "position": "float64",
-        })
+        return pd.DataFrame(all_data).astype(
+            {
+                "clicks": "int",
+                "impressions": "int",
+                "ctr": "float64",
+                "position": "float64",
+            }
+        )
